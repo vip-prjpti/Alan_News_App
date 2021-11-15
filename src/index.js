01c2c9662c8ba3a3af8496e7f41af6da8d6c2bf2
prajapativipul0408@gmail.com <prajapativipul0408@gmail.com>
2021-11-15 10:01:11 +0530

    Add Login Page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './index.css';
 
 import App from './App';
+import Login from './pages/Login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}></Route>
+                <Route path="/login" element={<Login />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
